8b1d2cc8bd9c6d0c1756c1f98a1fe6bf653d526a
Shubham Shubham <shubham.shubham@bahag.com>
2023-10-25 22:08:43 +0200

    Adds some more mock events and a card view for event listings.

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -14,6 +14,34 @@ const events = [
     location: 'Johns pizza',
     likes: 0,
   },
+  {
+    title: 'a mock event',
+    id: '51a7881c-aeb6-442b-85b7-2327cc080086',
+    description: 'something really cool',
+    location: 'Joes pizza',
+    likes: 0,
+  },
+  {
+    title: 'another mock event',
+    id: '2b699e91-b089-4745-a58d-d15667c79784',
+    description: 'something even cooler',
+    location: 'Johns pizza',
+    likes: 0,
+  },
+  {
+    title: 'a mock event',
+    id: '51a7881c-aeb6-442b-85b7-2327cc080086',
+    description: 'something really cool',
+    location: 'Joes pizza',
+    likes: 0,
+  },
+  {
+    title: 'another mock event',
+    id: '2b699e91-b089-4745-a58d-d15667c79784',
+    description: 'something even cooler',
+    location: 'Johns pizza',
+    likes: 0,
+  },
 ];
 @Component({
   selector: 'app-events-list',
