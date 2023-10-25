3d33f8046f3f4cf9b6bc73d052df898598de4072
Shubham Shubham <shubham.shubham@bahag.com>
2023-10-25 21:43:31 +0200

    Adds the basic routes for all components(pages) in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsListComponent } from './events/events-list.component';
+import { AboutComponent } from './about/about.component';
+import { EventComponent } from './events/event.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EventsListComponent
+  },
+  {
+    path: 'events',
+    component: EventsListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'event',
+    component: EventComponent
+  },
+  {
+    path: '**',
+    component: EventsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
