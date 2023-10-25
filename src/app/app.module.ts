e9ed6153b0b72cfda8b02163318b880334e6e526
Sham albukaee <albukaees@qr073rv4y1.de.bauhaus.intra>
2023-10-25 16:45:00 +0200

    Add event page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventComponent } from './events/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
