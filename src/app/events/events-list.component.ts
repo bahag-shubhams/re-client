8519f2007d92a99f6fc77eba80bcd6d22138b5e5
bahag-turans <sarper.turan@bahag.com>
2023-10-27 13:14:26 +0200

    Update styling and delete button

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faCalendar} from '@fortawesome/free-regular-svg-icons';
 
 interface Event {
   title: string;
@@ -10,13 +12,13 @@ interface Event {
 
 const events: Event[] = [
   {
-    title: 'Dance Class at Anthonys Place',
+    title: 'Dance Class at Anthonys',
     description: 'This event is a great opportunity for anyone who loves to dance or wants to learn how to dance. The event is held at Anthonys Place, which is a popular venue for dance classes and other events. The class is taught by experienced instructors who are passionate about dance and are dedicated to helping their students improve their skills. The event is open to people of all ages and skill levels, so whether youre a beginner or an experienced dancer, youll be able to enjoy the class and learn something new. The class covers a variety of dance styles, including salsa, tango, and ballroom dancing',
     location: 'Johns pizza',
     likes: 0,
   },
   {
-    title: 'Global Day of Respect 2023',
+    title: 'Global Day of Respect',
     description: 'Mandatory Attendance',
     location: 'Johns pizza',
     likes: 0,
@@ -56,6 +58,13 @@ export class EventsListComponent {
   showDetails(_t5: Event) {
     throw new Error('Method not implemented.');
   }
+
+  deleteEvent(_t5: Event) {
+    throw new Error('Method not implemented.')
+  }
+
+  faTrash = faTrash;
+  faCalendar = faCalendar;
 
   events = events;
 
