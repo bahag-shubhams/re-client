6a04eb7dbba47ead8767d2d6b4e69bf3f2c36997
bahag-turans <sarper.turan@bahag.com>
2023-11-13 14:16:30 +0100

    Add image url to event

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -6,6 +6,6 @@ export class Event {
         public dat: string,
         public eventid: number,
         public liked : number,
-        public imageUrl: string
+        public image_url: string
     ) {}
 }
