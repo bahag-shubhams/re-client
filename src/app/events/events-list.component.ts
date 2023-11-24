a23bdabdfcf15ab2d76c81d9e3a0c19dd9e5b43a
bahag-turans <sarper.turan@bahag.com>
2023-11-24 11:53:44 +0100

    Implement comment and sort functionalities

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -4,6 +4,8 @@ import { Event } from '../models/event';
 import { Router } from '@angular/router';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import {faCalendar} from '@fortawesome/free-regular-svg-icons';
+import { faSort } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
@@ -15,12 +17,11 @@ export class EventsListComponent implements OnInit {
   }
 
   events!: Event[];
-  deleteEvent(event: Event) {
-    this.eventService.deleteEvent(event.eventid).subscribe(() => this.getEvents());
-  }
-
+  searchTerm: string = ''
   faTrash = faTrash;
   faCalendar = faCalendar;
+  faSort = faSort;
+  isAscendingOrder: boolean = true;
 
   constructor(private eventService: EventService, private router: Router) {}
 
@@ -31,6 +32,36 @@ export class EventsListComponent implements OnInit {
   addEvent(event: Event): void {
     this.eventService.addEvent(event).subscribe(() => this.getEvents());
   }
+
+  deleteEvent(event: Event) {
+    this.eventService.deleteEvent(event.eventid).subscribe(() => this.getEvents());
+  }
+
+  filteredAndSortedEvents(): Event[] {
+    let filteredEvents = this.events.filter((event) =>
+      event.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+
+    return this.sortEvents(filteredEvents);
+  }
+
+  sortEvents(events: Event[]): Event[] {
+    return events.sort((a, b) => {
+      const dateA = new Date(a.dat).getTime();
+      const dateB = new Date(b.dat).getTime();
+
+      if (this.isAscendingOrder) {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    });
+  }
+
+  sortEventsByDate(): void {
+    this.isAscendingOrder = !this.isAscendingOrder;
+  }
+
 
   ngOnInit(): void {
     this.getEvents();
