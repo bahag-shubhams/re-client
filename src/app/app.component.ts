d7cafc9730f24d99f1166a10e99c6265e3d5b5d1
bahag-jasharidr <drilon.jashari@bahag.com>
2023-12-01 17:30:12 +0100

    Removing NavBar in SignUp and SingIn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouteStateService } from './services/route-state.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  showNavbar = true;
+  private subscription: Subscription = new Subscription();
+
+  constructor(private routeStateService: RouteStateService) {}
+
+  ngOnInit() {
+    this.subscription = this.routeStateService.isLoginRoute.subscribe(
+      (isLoginRoute) => {
+        this.showNavbar = !isLoginRoute;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   title = 're-client';
 }
+
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { RouteStateService } from '../route-state.service';
+// import { Subscription } from 'rxjs';
+
+// @Component({
+//   // component metadata here
+// })
+// export class NavbarComponent implements OnInit, OnDestroy {
+//   showNavbar = true;
+//   private subscription: Subscription;
+
+//   constructor(private routeStateService: RouteStateService) {}
+
+//   ngOnInit() {
+//     this.subscription = this.routeStateService.isLoginRoute.subscribe(isLoginRoute => {
+//       this.showNavbar = !isLoginRoute;
+//     });
+//   }
+
+//   ngOnDestroy() {
+//     this.subscription.unsubscribe();
+//   }
+// }
