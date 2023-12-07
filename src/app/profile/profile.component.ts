62878406eaa88db571b72f8d40556324b68c8c72
Shubham Shubham <shubham.shubham@bahag.com>
2023-12-07 16:32:37 +0100

    adds the favorite events in profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { User } from '../models/user';
+import { User, UserFavoriteEvent } from '../models/user';
+import { Event } from '../models/event';
+import { EventService } from '../events/event.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +10,38 @@ import { User } from '../models/user';
 })
 export class ProfileComponent {
   user!: User;
+  events: Event[] = [];
+  favoriteEvents: UserFavoriteEvent[] = [];
+
+  localUser: User  = {} as User;
+  userid : number = -1;
+
+
+
+  constructor(private eventService : EventService) { }
+
+  getEvents() {
+    this.eventService.getEvents().subscribe((events) => {
+      this.events = events;
+      this.eventService.getUserFavoriteEvents(this.userid).subscribe((favoriteEvents) => {
+        this.favoriteEvents = favoriteEvents;
+        this.events = this.events.filter((event) => {
+          return this.favoriteEvents.some((favoriteEvent) => {
+            return favoriteEvent.event_id === event.eventid;
+          });
+        });
+        console.log(this.events);
+      });
+    });
+
+    // filter the events to only show the ones that the user has favorited
+  }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.localUser = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : {} as User;
+    this.userid = this.localUser.userid;
+    this.getEvents();
   }
 
 }
